297f0202368a54682445a5ba18f01872e0670d12
Peeyush Yadav <peeyushy95@gmail.com>
2015-04-16 22:53:47 +0530

    ad-hoc
    Project Euler #7 10001st prime

---
+++ Project Euler #7 10001st prime.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+bool l;
+int a[108005];
+int main(){
+    int t,n,h;
+    long long j,i;
+    cin>>t;
+    a[1]=2;
+    a[2]=3;
+    h=3;
+    for(i=3;i<=104743;i+=2)
+    {
+        if(0!=i%3){
+            l=true;
+            for(j=2;j*j<=i;j++){
+                if(i%j==0){
+                    l=false;
+                    break;
+                }
+            }
+            if(l){
+               a[h++]=i; 
+            }
+            
+        }    
+    }
+    while(t--){
+        cin>>n;  
+        cout<<a[n]<<endl;
+    }
+    
+    return 0;
+}
