90b166f93e42b12455953cd5702058e6b0f75654
Peeyush Yadav <peeyushy95@gmail.com>
2015-04-18 15:49:15 +0530

    D.P
    Project Euler #14 Longest Collatz sequence

---
+++ Project Euler #14 Longest Collatz sequence.cpp
@@ -0,0 +1,99 @@
+/*
+   AUTHOR: Peeyush yadav
+   Problem:Project Euler #12: Highly divisible triangular number
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define f(a,b,c)                for(int a=b;a<c;a++)
+#define s(x)                    scanf("%d",&x);
+#define sl(x)                   scanf("%lld",&x);
+#define p(x)                    printf("%d\n",x);
+#define pl(x)                   printf("%lld\n",x);
+#define p1d(a,n)                for(int ix=0;ix<n;ix++) printf("%d ",a[ix]); printf("\n");
+#define p2d(a,n,m)              for(int ix=0;ix<n;ix++){ for(int jx=0;jx<m;jx++) printf("%d ",a[ix][jx]); printf("\n");}
+void input(){
+    #ifdef Megamind
+            #define DEBUG
+            #define TRACE
+            #define gc getchar()
+            freopen("inp.txt","r",stdin);
+            //freopen("out.txt","w",stdout);
+    #else
+            #define gc getchar_unlocked()
+    #endif
+}
+#ifdef TRACE
+    #define trace(x)                       cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<endl;
+    #define trace2(x,y)                    cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<endl;
+    #define trace3(x,y,z)                  cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<" | "#z" = "<<z<<endl;
+#else
+    #define trace(x)
+    #define trace2(x,y)
+    #define trace3(x,y,z) 
+#endif
+template <class T>
+inline void read(T &n1){
+	n1=0;
+    char c=gc;
+    while(c<'0' || c>'9') c=gc;
+    while(c>='0' && c<='9'){
+       n1=(n1<<3)+(n1<<1)+c-'0';
+       c=gc;
+    }
+}
+ 
+inline ll power(ll a, ll b, ll m) {
+    ll r = 1;
+    while(b) {
+        if(b & 1) r = r * a % m;
+        a = (a * a)% m;
+        b >>= 1;
+    }
+    return r;
+}
+inline ll power(ll a, ll b) {
+     ll r = 1;
+    while(b) {
+        if(b & 1) r = r * a ;
+        a = a * a;
+        b >>= 1;
+    }
+    return r;
+}
+ 
+/*........................................................END OF TEMPLATES.......................................................................*/
+int maxl[5000100],leng[5000010];
+#define MAXN 5000010
+
+void precompute(){
+	f(i,1,5000002){
+		ll num = i;
+		int counter = 1;
+		while(num!=1LL){
+			if(num<MAXN && leng[num]){
+				counter+= leng[num]-1;
+				break;				
+			}
+			if(num&1) num = 3*num +1;
+			else      num>>=1;
+			counter++;
+		}
+		leng[i]=counter;
+		maxl[i]= leng[maxl[i-1]]>counter? maxl[i-1]:i;
+	}	
+}
+int main(){
+	input();
+	precompute();	
+	int t,n;
+	s(t);
+	while(t--){
+		s(n);
+		p(maxl[n]);
+	}
+
+	#ifdef Megamind
+		cout << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC * 1000 << " ms." << endl;
+    #endif
+}
