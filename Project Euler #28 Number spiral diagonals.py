0b150281f6abd1838d523dfc69309378bf252823
Peeyush Yadav <peeyushy95@gmail.com>
2015-05-29 23:38:22 +0530

    Simple Math

---
+++ Project Euler #28 Number spiral diagonals.py
@@ -0,0 +1,10 @@
+# AUTHOR  : Peeyush Yadav
+# Problem : Project Euler #28: Number spiral diagonals
+
+T = int(raw_input())
+
+while T > 0 :
+        n = int(raw_input())
+        ans = (4*n*n*n + 3*n*n + 8*n - 9) / 6
+        print(ans%1000000007)
+        T = T - 1
