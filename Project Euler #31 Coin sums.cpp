fac59286187a132368663fd72b034b0319699100
Peeyush Yadav <peeyushy95@gmail.com>
2015-06-02 22:48:24 +0530

    Dynamic Programming

---
+++ Project Euler #31 Coin sums.cpp
@@ -0,0 +1,93 @@
+/*
+   AUTHOR  : Peeyush Yadav
+   Problem : Project Euler #31: Coin sums
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define f(a,b,c)                for(int a=b;a<c;a++)
+#define s(x)                    scanf("%d",&x);
+#define sl(x)                   scanf("%lld",&x);
+#define p(x)                    printf("%d\n",x);
+#define pl(x)                   printf("%lld\n",x);
+#define p1d(a,n)                for(int ix=0;ix<n;ix++) printf("%d ",a[ix]); printf("\n");
+#define p2d(a,n,m)              for(int ix=0;ix<n;ix++){ for(int jx=0;jx<m;jx++) printf("%d ",a[ix][jx]); printf("\n");}
+void input(){
+    #ifdef Megamind
+            #define DEBUG
+            #define TRACE
+            #define gc getchar()
+            freopen("inp.txt","r",stdin);
+            //freopen("out1.txt","w",stdout);
+    #else
+            #define gc getchar_unlocked()
+    #endif
+}
+#ifdef TRACE
+    #define trace(x)                       cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<endl;
+    #define trace2(x,y)                    cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<endl;
+    #define trace3(x,y,z)                  cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<" | "#z" = "<<z<<endl;
+#else
+    #define trace(x)
+    #define trace2(x,y)
+    #define trace3(x,y,z) 
+#endif
+template <class T>
+inline void read(T &n1){
+	n1=0;
+    char c=gc;
+    while(c<'0' || c>'9') c=gc;
+    while(c>='0' && c<='9'){
+       n1=(n1<<3)+(n1<<1)+c-'0';
+       c=gc;
+    }
+}
+inline ll power(ll a, ll b, ll m) {
+    ll r = 1;
+    while(b) {
+        if(b & 1) r = r * a % m;
+        a = (a * a)% m;
+        b >>= 1;
+    }
+    return r;
+}
+inline ll power(ll a, ll b) {
+     ll r = 1;
+    while(b) {
+        if(b & 1) r = r * a ;
+        a = a * a;
+        b >>= 1;
+    }
+    return r;
+}
+ 
+/*........................................................END OF TEMPLATES.......................................................................*/
+#define SIZE 1000000 + 5
+#define mod (ll)(1e9 + 7)
+ll pre[SIZE];
+
+int main(){
+	input();
+	
+	int use[] = {1,2,5,10,20,50,100,200};
+	pre[0]=1;
+	
+	f(x,0,8)
+		f(i,1,SIZE-1) 
+			if( i-use[x] >= 0 && pre[i - use[x]])
+					pre[i] = (pre[i] + pre[i - use[x]])%mod;
+				
+	int t;
+	s(t);
+	
+	while(t--){
+		int n;
+		s(n);
+		pl(pre[n]);
+	}
+	
+	#ifdef Megamind
+		cout << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC * 1000 << " ms." << endl;
+    #endif
+}
