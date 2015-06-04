0214035e4499b949b2d0ccbd54f007df54554b58
Peeyush Yadav <peeyushy95@gmail.com>
2015-06-04 22:06:29 +0530

    Ad-hoc

---
+++ Project Euler #33 Digit canceling fractions.cpp
@@ -0,0 +1,99 @@
+/*
+   AUTHOR  : Peeyush Yadav
+   Problem : Project Euler #33: Digit canceling fractions
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define f(a,b,c)                for(int a=b;a<c;a++)
+#define s(x)                    scanf("%d",&x);
+#define sl(x)                   scanf("%lld",&x);
+#define p(x)                    printf("%d\n",x);
+#define p2(x,y)                 printf("%d %d\n",x,y);
+#define pl(x)                   printf("%lld\n",x);
+#define pl2(x,y)                 printf("%lld %lld\n",x,y);
+#define p1d(a,n)                for(int ix=0;ix<n;ix++) printf("%d ",a[ix]); printf("\n");
+#define p2d(a,n,m)              for(int ix=0;ix<n;ix++){ for(int jx=0;jx<m;jx++) printf("%d ",a[ix][jx]); printf("\n");}
+void input(){
+    #ifdef Megamind
+            #define DEBUG
+            #define TRACE
+            #define gc getchar()
+            freopen("inp.txt","r",stdin);
+            //freopen("out1.txt","w",stdout);
+    #else
+            #define gc getchar_unlocked()
+    #endif
+}
+#ifdef TRACE
+    #define trace(x)                       cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<endl;
+    #define trace2(x,y)                    cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<endl;
+    #define trace3(x,y,z)                  cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<" | "#z" = "<<z<<endl;
+#else
+    #define trace(x)
+    #define trace2(x,y)
+    #define trace3(x,y,z) 
+#endif
+template <class T>
+inline void read(T &n1){
+	n1=0;
+    char c=gc;
+    while(c<'0' || c>'9') c=gc;
+    while(c>='0' && c<='9'){
+       n1=(n1<<3)+(n1<<1)+c-'0';
+       c=gc;
+    }
+}
+inline ll power(ll a, ll b, ll m) {
+    ll r = 1;
+    while(b) {
+        if(b & 1) r = r * a % m;
+        a = (a * a)% m;
+        b >>= 1;
+    }
+    return r;
+}
+inline ll power(ll a, ll b) {
+     ll r = 1;
+    while(b) {
+        if(b & 1) r = r * a ;
+        a = a * a;
+        b >>= 1;
+    }
+    return r;
+}
+ 
+/*........................................................END OF TEMPLATES.......................................................................*/
+
+
+ll a[5][4][2];
+
+int main(){
+	input();
+	
+	int n,k;
+	s(n);
+	s(k);
+	
+	a[2][1][0] = 110;
+	a[2][1][1] = 322;
+	
+	a[3][1][0] = 77262;
+	a[3][1][1] = 163829;
+	a[3][2][0] = 7429;
+	a[3][2][1] = 17305;
+	
+	a[4][1][0] = 12999936;
+	a[4][1][1] = 28131911;
+	a[4][2][0] = 3571225;
+	a[4][2][1] = 7153900;
+	a[4][3][0] = 255983;
+	a[4][3][1] = 467405;
+	
+	pl(a[n][k][0],a[n][k][1]);
+	
+	#ifdef Megamind
+		cout << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC * 1000 << " ms." << endl;
+    #endif
+}
