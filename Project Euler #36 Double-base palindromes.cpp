c152530dc9456e6b6983393d624593271e62eec3
Peeyush Yadav <peeyushy95@gmail.com>
2015-06-05 16:30:51 +0530

    Ad-hoc

---
+++ Project Euler #36 Double-base palindromes.cpp
@@ -0,0 +1,113 @@
+/*
+   AUTHOR  : Peeyush Yadav
+   Problem : Project Euler #36: Double-base palindromes
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define f(a,b,c)                for(int a=b;a<c;a++)
+#define s(x)                    scanf("%d",&x);
+#define sl(x)                   scanf("%lld",&x);
+#define p(x)                    printf("%d\n",x);
+#define p2(x,y)                 printf("%d %d\n",x,y);
+#define pl(x)                   printf("%lld\n",x);
+#define pl2(x,y)                 printf("%lld %lld\n",x,y);
+#define p1d(a,n)                for(int ix=0;ix<n;ix++) printf("%d ",a[ix]); printf("\n");
+#define p2d(a,n,m)              for(int ix=0;ix<n;ix++){ for(int jx=0;jx<m;jx++) printf("%d ",a[ix][jx]); printf("\n");}
+void input(){
+    #ifdef Megamind
+            #define DEBUG
+            #define TRACE
+            #define gc getchar()
+            freopen("inp.txt","r",stdin);
+            //freopen("out1.txt","w",stdout);
+    #else
+            #define gc getchar_unlocked()
+    #endif
+}
+#ifdef TRACE
+    #define trace(x)                       cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<endl;
+    #define trace2(x,y)                    cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<endl;
+    #define trace3(x,y,z)                  cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<" | "#z" = "<<z<<endl;
+#else
+    #define trace(x)
+    #define trace2(x,y)
+    #define trace3(x,y,z) 
+#endif
+template <class T>
+inline void read(T &n1){
+	n1=0;
+    char c=gc;
+    while(c<'0' || c>'9') c=gc;
+    while(c>='0' && c<='9'){
+       n1=(n1<<3)+(n1<<1)+c-'0';
+       c=gc;
+    }
+}
+inline ll power(ll a, ll b, ll m) {
+    ll r = 1;
+    while(b) {
+        if(b & 1) r = r * a % m;
+        a = (a * a)% m;
+        b >>= 1;
+    }
+    return r;
+}
+inline ll power(ll a, ll b) {
+     ll r = 1;
+    while(b) {
+        if(b & 1) r = r * a ;
+        a = a * a;
+        b >>= 1;
+    }
+    return r;
+}
+ 
+/*........................................................END OF TEMPLATES.......................................................................*/
+int a[1000];
+bool palin10(int i){
+	int ind = 0;
+	while(i){
+		a[ind++]= i%10;
+		i/=10;
+	}
+	
+	f(i,0,ind/2){
+		if(a[i] != a[ind-i-1]) return 0;
+	}
+	return 1;
+}
+
+bool palink(int i,int k){
+	int ind = 0;
+	while(i){
+		a[ind++]= i%k;
+		i/=k;
+	}
+	
+	f(i,0,ind/2){
+		if(a[i] != a[ind-i-1]) return 0;
+	}
+	return 1;
+}
+
+
+int main(){
+	input();
+	
+	int n,k;
+	s(n);
+	s(k);
+	ll ans = 0;
+	
+	f(i,1,n){
+		if(palin10(i) && palink(i,k)) ans+=(ll)i;
+	}
+	
+	pl(ans)
+	
+	#ifdef Megamind
+		cout << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC * 1000 << " ms." << endl;
+    #endif
+}
