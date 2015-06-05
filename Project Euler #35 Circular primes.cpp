c6b2cd528701363ba10123ff4a109a342fbe7fb7
Peeyush Yadav <peeyushy95@gmail.com>
2015-06-05 16:15:25 +0530

    Prime

---
+++ Project Euler #35 Circular primes.cpp
@@ -0,0 +1,136 @@
+/*
+   AUTHOR  : Peeyush Yadav
+   Problem : Project Euler #35: Circular primes
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define f(a,b,c)                for(int a=b;a<c;a++)
+#define s(x)                    scanf("%d",&x);
+#define sl(x)                   scanf("%lld",&x);
+#define p(x)                    printf("%d\n",x);
+#define p2(x,y)                 printf("%d %d\n",x,y);
+#define pl(x)                   printf("%lld\n",x);
+#define pl2(x,y)                 printf("%lld %lld\n",x,y);
+#define p1d(a,n)                for(int ix=0;ix<n;ix++) printf("%d ",a[ix]); printf("\n");
+#define p2d(a,n,m)              for(int ix=0;ix<n;ix++){ for(int jx=0;jx<m;jx++) printf("%d ",a[ix][jx]); printf("\n");}
+void input(){
+    #ifdef Megamind
+            #define DEBUG
+            #define TRACE
+            #define gc getchar()
+            freopen("inp.txt","r",stdin);
+            //freopen("out1.txt","w",stdout);
+    #else
+            #define gc getchar_unlocked()
+    #endif
+}
+#ifdef TRACE
+    #define trace(x)                       cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<endl;
+    #define trace2(x,y)                    cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<endl;
+    #define trace3(x,y,z)                  cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<" | "#z" = "<<z<<endl;
+#else
+    #define trace(x)
+    #define trace2(x,y)
+    #define trace3(x,y,z) 
+#endif
+template <class T>
+inline void read(T &n1){
+	n1=0;
+    char c=gc;
+    while(c<'0' || c>'9') c=gc;
+    while(c>='0' && c<='9'){
+       n1=(n1<<3)+(n1<<1)+c-'0';
+       c=gc;
+    }
+}
+inline ll power(ll a, ll b, ll m) {
+    ll r = 1;
+    while(b) {
+        if(b & 1) r = r * a % m;
+        a = (a * a)% m;
+        b >>= 1;
+    }
+    return r;
+}
+inline ll power(ll a, ll b) {
+     ll r = 1;
+    while(b) {
+        if(b & 1) r = r * a ;
+        a = a * a;
+        b >>= 1;
+    }
+    return r;
+}
+ 
+/*........................................................END OF TEMPLATES.......................................................................*/
+int n;
+bool p[1000005];
+void seive(){
+    p[1]=p[0]=1;
+	int j;
+	for(int i=2;i*i<=(1000005);i++){
+		if(!p[i]){
+			j = i+i;
+			while(j<=(1000005)){
+				p[j]=1;
+				j+=i;
+			}
+		}
+	}
+}
+
+ll ans[1000005];
+
+void check(){
+	bool flag = 1;
+	int ind = 0,temp,digit,base,k;
+	f(i,2,n){
+		
+		ans[i] = ans[i-1];
+		
+		if(!p[i]){
+			
+			flag = 1;
+			ind = 0;
+			
+			temp = i;
+			base  = 1;
+			digit = 0;
+			
+			while(temp/base){
+				digit++;
+				base*=10;
+			}
+			
+			temp = i;
+			
+			f(iter,1,digit){
+				temp*=10;
+				k = temp/base;
+				temp = temp%base + k;
+				
+				if(p[temp]){
+					flag = 0;
+					break;
+				}
+			}
+			if(flag) 	ans[i]+= (ll)i;
+			
+			
+		}
+	}
+}
+int main(){
+	input();
+	
+	s(n);
+	seive();
+	check();
+	pl(ans[n-1]);
+	
+	#ifdef Megamind
+		cout << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC * 1000 << " ms." << endl;
+    #endif
+}
