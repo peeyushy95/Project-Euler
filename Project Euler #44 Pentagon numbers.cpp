e109f43b7c315f5227f6da1bfa274bcad56c81de
Peeyush Yadav <peeyushy95@gmail.com>
2015-06-09 13:16:52 +0530

    Set

---
+++ Project Euler #44 Pentagon numbers.cpp
@@ -0,0 +1,81 @@
+/*
+   AUTHOR  : Peeyush Yadav
+   Problem : Project Euler #44: Pentagon numbers
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define f(a,b,c)                for(int a=b;a<c;a++)
+#define s(x)                    scanf("%d",&x);
+#define sl(x)                   scanf("%lld",&x);
+#define p(x)                    printf("%d\n",x);
+#define p2(x,y)                 printf("%d %d\n",x,y);
+#define pl(x)                   printf("%lld\n",x);
+#define pl2(x,y)                printf("%lld %lld\n",x,y);
+#define p1d(a,n)                for(int ix=0;ix<n;ix++) printf("%d ",a[ix]); printf("\n");
+#define p2d(a,n,m)              for(int ix=0;ix<n;ix++){ for(int jx=0;jx<m;jx++) printf("%d ",a[ix][jx]); printf("\n");}
+void input(){
+    #ifdef Megamind
+            #define DEBUG
+            #define TRACE
+            freopen("inp.txt","r",stdin);
+            //freopen("out1.txt","w",stdout);
+    #endif
+}
+#ifdef TRACE
+    #define trace(x)                       cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<endl;
+    #define trace2(x,y)                    cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<endl;
+    #define trace3(x,y,z)                  cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<" | "#z" = "<<z<<endl;
+#else
+    #define trace(x)
+    #define trace2(x,y)
+    #define trace3(x,y,z) 
+#endif
+
+inline ll power(ll a, ll b, ll m) {
+    ll r = 1;
+    while(b) {
+        if(b & 1) r = r * a % m;
+        a = (a * a)% m;
+        b >>= 1;
+    }
+    return r;
+}
+inline ll power(ll a, ll b) {
+     ll r = 1;
+    while(b) {
+        if(b & 1) r = r * a ;
+        a = a * a;
+        b >>= 1;
+    }
+    return r;
+}
+ 
+/*........................................................END OF TEMPLATES.......................................................................*/
+
+set<ll> myset;
+ll a[1000008];
+int main(){
+	input();
+	ll n,k;
+	sl(n);
+	sl(k);
+	
+	f(i,1,n){
+		a[i] =  ((ll)i*(3*(ll)i-1))/2;
+		myset.insert(a[i]);
+	}
+	
+	f(i,1,n){
+				
+		if(i-k > 0)
+			if(myset.find(a[i]-a[i-k]) != myset.end()  || myset.find(a[i]+a[i-k]) != myset.end()  )
+						pl(a[i]);
+		
+	}
+	
+	#ifdef Megamind
+		cout << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC * 1000 << " ms." << endl;
+    #endif
+}
